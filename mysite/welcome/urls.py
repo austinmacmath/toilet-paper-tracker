54441f6ccdd614692988cc1daa2e55f6c2e0f39a
Austin Mac <austinmac@ucsb.edu>
2020-03-28 02:56:31 -0700

    delete comments

--- mysite/welcome/urls.py
+++ mysite/welcome/urls.py
@@ -7,8 +7,5 @@ urlpatterns = [
     path('<int:pk>/', views.DetailView.as_view(), name = 'detail'),
     path('<int:pk>/name/', views.NameView.as_view(), name = 'name'),
     path('<int:donor_id>/points/', views.points, name = 'points'),
-    # path('<int:donor_id/recipients/', views.recipients, name = 'recipients'),
-    # path('<int:donor_id/results/', views.results, name = 'results'),
-    # path('search/', views.search, name='search'),
     path('search/', views.SearchResultView.as_view(), name='searchresult')
 ]
